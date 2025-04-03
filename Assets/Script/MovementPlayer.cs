055c31099a3dbf09496bb7dfb047d0777c4fb1df
David Ezequiel Escalada <metalcombi17@gmail.com>
2025-04-03 12:44:00 -0300

    code bombs spawn y collider
    el spawn funciona, pero hay que arreglar el choque entre la bomba y el jugador.

--- Assets/Script/MovementPlayer.cs
+++ Assets/Script/MovementPlayer.cs
@@ -6,7 +6,9 @@ public class MovementPlayer : MonoBehaviour
 {
     private CharacterController characterController;
 
-    public float Speed = 5f;
+    public float speed = 5f;
+    public GameObject bombPrefab;
+    private GameObject lastBomb;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -15,8 +17,53 @@ public class MovementPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        SpawnBomb();
+
+        if (lastBomb != null && Vector3.Distance(transform.position, lastBomb.transform.position) > 1.0f)
+        {
+            EnableBombCollision();  
+        }
+    }
+
+    private void Move()
+    {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        
-        characterController.Move(move*Time.deltaTime*Speed);
+
+        characterController.Move(move * Time.deltaTime * speed);
+    } 
+
+    private void SpawnBomb()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           lastBomb = Instantiate(bombPrefab, transform.position, Quaternion.identity);
+           Collider bombCollider = lastBomb.GetComponent<Collider>();
+           Collider playerCollider = GetComponent<Collider>();
+
+            if (bombCollider != null && playerCollider != null)
+            {
+                Physics.IgnoreCollision(bombCollider, playerCollider, true);
+            }
+        }
+
+      
+
+    }
+
+    private void EnableBombCollision()
+    {
+        if (lastBomb != null)
+        {
+            Collider bombCollider = lastBomb.GetComponent<Collider>();
+            Collider playerCollider = GetComponent<Collider>();
+
+            if (bombCollider != null && playerCollider != null)
+            {
+                Physics.IgnoreCollision(bombCollider, playerCollider, false);
+            }
+
+            lastBomb = null;
+        }
     }
 }
